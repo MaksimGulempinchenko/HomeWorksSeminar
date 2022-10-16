75068424bf3abc587469b9f49c428e6ad5386979
Gulempinchenko Maksim <gulempin@gmail.com>
2022-10-16 04:12:58 +0300

    complete example 3

--- homeworks_1/Program.cs
+++ homeworks_1/Program.cs
@@ -14,27 +14,49 @@
 // }
 
 
+
+
+
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.WriteLine("input three nubers: ");
-int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
-int c = Convert.ToInt32(Console.ReadLine());
-int max;
-if(a > b)
-{
-     max = a;
-}
-else 
-{
-    max = b;
-}
+// Console.WriteLine("input three nubers: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = Convert.ToInt32(Console.ReadLine());
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max;
+// if(a > b)
+// {
+//      max = a;
+// }
+// else 
+// {
+//     max = b;
+// }
 
-if(max > c)
-{
-Console.Write($"The biggest number is {max}");
-}
-else
-{
-Console.Write($"The biggest numbers is {c} ");
-}
+// if(max > c)
+// {
+// Console.Write($"The biggest number is {max}");
+// }
+// else
+// {
+// Console.Write($"The biggest numbers is {c} ");
+// }
+
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.Write("Input a number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// if( a % 2 == 1)
+// {
+//     Console.Write($"{a} is an odd integer ");
+// }
+// else
+// {
+//     Console.Write($"{a} is an even integer ");
+// }
+
+
+
