37daf0102bc33f70b89b850dd16a73dd27c3d9ec
Gulempinchenko Maksim <gulempin@gmail.com>
2022-10-16 03:26:04 +0300

    finish exampls one

---
+++ homeworks_1/Program.cs
@@ -0,0 +1,18 @@
+﻿//   Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("Input two numbers: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = Convert.ToInt32(Console.ReadLine());
+// if( a > b )
+// {
+//     Console.Write($" number {a} is bigger than number {b}" );
+
+// }
+// else
+// {
+//     Console.Write($" number {b} is bigger than number {a} ");
+// }
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
