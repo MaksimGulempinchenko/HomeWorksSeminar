cd5eac519b8480c0c464c0a4b00052f9d9465c4f
Gulempinchenko Maksim <gulempin@gmail.com>
2022-10-19 14:14:38 +0300

    complete task 1

---
+++ homework_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+
+// int Number(int num)
+// {
+//     int des = num % 100;
+//     int ed = des / 10;
+// return ed;
+// }
+
+
+// Console.WriteLine("Input three - digit");
+// int threeDigit = Convert.ToInt32(Console.ReadLine());
+// int answer = Number(threeDigit);
+// Console.WriteLine(answer);
