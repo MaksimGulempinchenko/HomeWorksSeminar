75099ff8020555ff8a93c9c1a3f0fc77887a7a53
Gulempinchenko Maksim <gulempin@gmail.com>
2022-12-02 13:40:43 +0300

    complete

---
+++ homework_9/Program.cs
@@ -0,0 +1,41 @@
+﻿//     Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// void ShowNum(int n )
+// {   
+//     Console.Write(n + " ");
+//     if(n > 1) ShowNum(n - 1);
+    
+// }
+
+// ShowNum(5);
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// int SumOfDigits(int m, int n)
+// {
+//     if( n > m )
+//     return SumOfDigits( m, n - 1) + n;
+//     return n;
+   
+// }
+// Console.Write(SumOfDigits(4 , 8));
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// int AckermannFunction (int numberM, int numberN)
+// {
+//     if (numberM == 0) return numberN + 1;
+//     if (numberM != 0 && numberN == 0) return AckermannFunction(numberM - 1, 1);
+//     if (numberM > 0 && numberN > 0) return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
+// return AckermannFunction(numberM, numberN);
+// }
+
+// Console.WriteLine(AckermannFunction(2,3));
